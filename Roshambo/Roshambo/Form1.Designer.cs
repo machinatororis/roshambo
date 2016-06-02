6f1584e470497c69f7b644a631f683dd762496c4
Masha <MashaLypa@yandex.ua>
2016-06-02 12:18:41 +0300

    фиксированный размер окна

--- Roshambo/Roshambo/Form1.Designer.cs
+++ Roshambo/Roshambo/Form1.Designer.cs
@@ -274,6 +274,7 @@
             this.Controls.Add(this.userPanelRock);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Roshambo";
